77c65c53ae64f1a1285798f205b8f1001f3e25be
Christian Fei <crifei93@gmail.com>
2017-02-19 18:35:27 +0100

    refactoring

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 'use strict'
-module.exports = function nextText (originalString, options = {}, currentString = '') {
+module.exports = nextText
+
+function nextText (originalString, options = {}, currentString = '') {
+  const next = nextFor(originalString, options, currentString)
+  const toString = () => currentString
+
   return {
     toString,
     next
   }
+}
 
-  function next () {
-    var remainingText = originalString.slice(currentString.length)
-    var finishedTyping = remainingText.length === 0
-    if (finishedTyping) {
-      return nextText(originalString, options, options.restart ? '' : originalString)
-    }
-    return nextText(originalString, options, currentString + remainingText[0])
+function nextFor (originalString, options, currentString) {
+  var remainingText = originalString.slice(currentString.length)
+  var finishedTyping = remainingText.length === 0
+  if (finishedTyping) {
+    return () => nextText(originalString, options, options.restart ? '' : originalString)
   }
-
-  function toString () {
-    return currentString
-  }
+  return () => nextText(originalString, options, currentString + remainingText[0])
 }
