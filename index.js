321f625588d8c2de9ce813726427bb044bf07652
Christian Fei <crifei93@gmail.com>
2018-06-01 07:22:55 +0200

    refactor with default params

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 module.exports = nextText
 
-function nextText (originalString, options, currentString) {
-  originalString = originalString || ''
-  options = options || {}
-  currentString = currentString || ''
+function nextText (originalString = '', options = {}, currentString = '') {
   return Object.assign(currentString, {
     next: nextFor(originalString, options, currentString),
     reset: function () { return nextText(originalString, options, '') }
